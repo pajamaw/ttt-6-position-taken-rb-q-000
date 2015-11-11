c2411f05d9e6784571ffee4e92371dc11fdd024c
pajamaw <prw42@cornell.edu>
2015-11-11 17:11:25 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
-# code your #position_taken? method here!
+def position_taken?(board, location)
+  if board[location] == "X"
+    return true
+  elsif board[location] == "O"
+    return true
+  elsif board[location] == ""
+    return false
+  elsif board[location] == " "
+    return false
+  else
+    return false
+  end
+end
